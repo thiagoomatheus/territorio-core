c12c2a82bfe12906f99dfc5476b299ce54d331c4
thiagoomatheus <thiagomatheus2001@hotmail.com>
2026-01-28 11:08:54 -0300

    feat(api): adiciona index para rotas

---
+++ packages/api/src/index.ts
@@ -0,0 +1,19 @@
+import { router } from './trpc';
+import { authRouter } from './router/auth';
+import { congregationRouter } from './router/congregation';
+import { territoryRouter } from './router/territory';
+import { teamRouter } from './router/team';
+import { assignmentRouter } from './router/assignments';
+import { managersRouter } from './router/manager';
+
+export const appRouter = router({
+  auth: authRouter,
+  congregation: congregationRouter,
+  territory: territoryRouter,
+  team: teamRouter,
+  assignment: assignmentRouter,
+  managers: managersRouter,
+});
+
+export type AppRouter = typeof appRouter;
+export { router, publicProcedure, protectedProcedure, authenticatedProcedure, type Context } from './trpc';
